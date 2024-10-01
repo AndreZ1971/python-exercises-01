7b720af3f3866c8411658266036dabb693de1f6e
AndreZ1971 <andre.zabel71@gmail.com>
2024-10-01 14:01:40 +0200

    Übungsaufgabe 2. erfolgreich erledigt

---
+++ exercise_02.py
@@ -0,0 +1,12 @@
+# Definiere Variablen
+a = 10
+b = 3
+
+# Mathematische Operationen
+print("Summe:", a + b)
+print("Differenz:", a - b)
+print("Produkt:", a * b)
+print("Ganzzahliger Quotient:", a // b)
+print("Rest der Division:", a % b)
+print("a hoch b:", a ** b)
+
