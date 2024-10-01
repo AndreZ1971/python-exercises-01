5d63a7d89ca9a1496e2709613d003aaf9d099c60
AndreZ1971 <andre.zabel71@gmail.com>
2024-10-01 13:52:53 +0200

    Übungsaufgabe 1. erfolgreich erledigt

---
+++ exercise_01.py
@@ -0,0 +1,12 @@
+# Definiere Variablen
+first_name = "Andre"  # Ersetze durch deinen Vornamen
+last_name = "Zabel"   # Ersetze durch deinen Nachnamen
+birth_year = 1971     # Ersetze durch dein Geburtsjahr
+has_license = False    # Ersetze mit True oder False je nach Führerscheinstatus
+
+# Ausgabe der Variablen
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("Führerschein:", has_license)
+
