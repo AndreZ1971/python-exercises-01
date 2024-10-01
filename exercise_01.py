722e27737abef671e3772ee33c55e566aae497a8
AndreZ1971 <andre.zabel71@gmail.com>
2024-10-01 14:36:04 +0200

    re new exercise_01

--- exercise_01.py
+++ exercise_01.py
@@ -1,8 +1,8 @@
 # Definiere Variablen
-first_name = "Andre"  # Ersetze durch deinen Vornamen
-last_name = "Zabel"   # Ersetze durch deinen Nachnamen
-birth_year = 1971     # Ersetze durch dein Geburtsjahr
-has_license = False    # Ersetze mit True oder False je nach Führerscheinstatus
+first_name = "Andre"  # Vornamen
+last_name = "Zabel"   # Nachnamen
+birth_year = 1971     # Geburtsjahr
+has_license = Fals    # Führerscheinstatus
 
 # Ausgabe der Variablen
 print("Vorname:", first_name)
